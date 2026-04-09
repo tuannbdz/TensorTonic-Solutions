78ad58852572b0791b87ae507594c9c30358d7d8
Bao-Tuan Nguyen <88710207+tuannbdz@users.noreply.github.com>
2026-04-09 10:31:51 +0700

    Add solution for leaky-relu

---
+++ leaky-relu/leaky-relu.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def leaky_relu(x, alpha=0.01):
+    """
+    Vectorized Leaky ReLU implementation.
+    """
+    # Write code here
+    x = np.array(x)
+    x = np.maximum(x, alpha * x)
+    return x
